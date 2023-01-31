61ac6aba7b9b6542ef098dbeaa896219de875ec2
dinesh2003 <adhikaridinesh76@gmail.com>
2023-01-30 23:06:41 -0500

    1/30

--- src/ProductGenerater.java
+++ src/ProductGenerater.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 
-public class ProductWriter
+public class ProductGenerater
 {
     public static void main(String[] args)
     {
